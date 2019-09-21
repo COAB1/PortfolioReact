26d1329cda82c79223d718dbf80f544952ea5a2f
COAB1 <carol.o.ag@gmail.com>
2019-09-21 17:04:55 -0300

    Updates

---
+++ precache-manifest.8a11bced259e01b99c7d08b2b1778b48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4748d1ecf84d6f13457d589c2f793abc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d7204a56946b40f6cdc",
+    "url": "/static/css/main.f74d8bbe.chunk.css"
+  },
+  {
+    "revision": "61c8c26cbecc4d327b28",
+    "url": "/static/js/2.8d6ee4ec.chunk.js"
+  },
+  {
+    "revision": "2d7204a56946b40f6cdc",
+    "url": "/static/js/main.a708416a.chunk.js"
+  },
+  {
+    "revision": "3c2210b84b35e0ca614f",
+    "url": "/static/js/runtime-main.7302273d.js"
+  },
+  {
+    "revision": "413064cf590e2ece85e6b0548ba878a0",
+    "url": "/static/media/eu.413064cf.jpg"
+  }
+]);
